b99a6cef1af2cf16e56e487193b02e2ee37e6a47
cfandoh123 <138801418+cfandoh123@users.noreply.github.com>
2023-10-27 17:59:18 -0400

    MultiPerceptron.java

--- MultiPerceptron.java
+++ MultiPerceptron.java
@@ -1,102 +1,69 @@
-import java.util.Random;
-
 public class MultiPerceptron {
-    private int numberOfClasses;
-    private int numberOfInputs;
-    private Perceptron[] perceptrons;
+
+    private Perceptron[] perceptrons; // perceptron object for each set of input
 
     // Creates a multi-perceptron object with m classes and n inputs.
+    // It creates an array of m perceptrons, each with n inputs.
     public MultiPerceptron(int m, int n) {
-        numberOfClasses = m;
-        numberOfInputs = n;
         perceptrons = new Perceptron[m];
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++)
             perceptrons[i] = new Perceptron(n);
-        }
     }
 
     // Returns the number of classes m.
     public int numberOfClasses() {
-        return numberOfClasses;
+        return perceptrons.length;
     }
 
-    // Returns the number of inputs n.
+    // Returns the number of inputs n (length of the feature vector).
     public int numberOfInputs() {
-        return numberOfInputs;
+        return perceptrons[0].numberOfInputs();
     }
 
     // Returns the predicted label (between 0 and m-1) for the given input.
     public int predictMulti(double[] x) {
-        Random random = new Random();
-        int predictedLabel = 0;
-        double maxWeightedSum = perceptrons[0].weightedSum(x);
-
-        for (int i = 1; i < numberOfClasses; i++) {
-            double weightedSum = perceptrons[i].weightedSum(x);
-            if (weightedSum > maxWeightedSum) {
-                maxWeightedSum = weightedSum;
-                predictedLabel = i;
-            } else if (weightedSum == maxWeightedSum) {
-                // If there's a tie, choose randomly among tied perceptrons.
-                if (random.nextBoolean()) {
-                    predictedLabel = i;
-                }
-            }
-        }
-
-        return predictedLabel;
+        int maxWeightClassIndex = 0;
+        for (int i = 1; i < numberOfClasses(); i++)
+            if (perceptrons[maxWeightClassIndex].weightedSum(x) <
+                    perceptrons[i].weightedSum(x))
+                maxWeightClassIndex = i;
+        return maxWeightClassIndex;
     }
 
     // Trains this multi-perceptron on the labeled (between 0 and m-1) input.
     public void trainMulti(double[] x, int label) {
-        perceptrons[label].train(x, 1);
-        for (int i = 0; i < numberOfClasses; i++) {
-            if (i != label) {
+        // int predictedClassIndex = predictMulti(x);
+        for (int i = 0; i < numberOfClasses(); i++) {
+            // int predictedClassIndex = predictMulti(x);
+            if (label == i)
+                perceptrons[i].train(x, +1);
+            else
                 perceptrons[i].train(x, -1);
-            }
+
         }
     }
 
-    // Returns a String representation of this MultiPerceptron.
+    // Returns a String representation of this MultiPerceptron, with
+    // the string representations of the perceptrons separated by commas
+    // and enclosed in parentheses.
+    // Example with m = 2 and n = 3: ((2.0, 0.0, -2.0), (3.0, 4.0, 5.0))
     public String toString() {
-        StringBuilder sb = new StringBuilder("(");
-        for (int i = 0; i < numberOfClasses; i++) {
-            sb.append(perceptrons[i].toString());
-            if (i < numberOfClasses - 1) {
-                sb.append(", ");
-            }
-        }
-        sb.append(")");
-        return sb.toString();
+        StringBuilder repr = new StringBuilder("(" + perceptrons[0]);
+        for (int i = 1; i < this.numberOfClasses(); i++)
+            repr.append(", " + perceptrons[i]);
+        repr.append(")");
+        return repr.toString();
     }
 
     // Tests this class by directly calling all instance methods.
     public static void main(String[] args) {
-        int m = 2;
-        int n = 3;
-
-        double[] training1 = {  3.0,  4.0,  5.0 };  // class 1
-        double[] training2 = {  2.0,  0.0, -2.0 };  // class 0
-        double[] training3 = { -2.0,  0.0,  2.0 };  // class 1
-        double[] training4 = {  5.0,  4.0,  3.0 };  // class 0
-
-        MultiPerceptron perceptron = new MultiPerceptron(m, n);
-        System.out.println(perceptron);
-        perceptron.trainMulti(training1, 1);
-        System.out.println(perceptron);
-        perceptron.trainMulti(training2, 0);
-        System.out.println(perceptron);
-        perceptron.trainMulti(training3, 1);
-        System.out.println(perceptron);
-        perceptron.trainMulti(training4, 0);
-        System.out.println(perceptron);
-
         double[] testing1 = { -1.0, -2.0, 3.0 };
         double[] testing2 = { 2.0, -5.0, 1.0 };
 
-        System.out.println(perceptron.predictMulti(testing1));
-        System.out.println(perceptron);
-        System.out.println(perceptron.predictMulti(testing2));
-        System.out.println(perceptron);
+        StdOut.println(perceptron.predictMulti(testing1));
+        StdOut.println(perceptron);
+        StdOut.println(perceptron.predictMulti(testing2));
+        StdOut.println(perceptron);
+
     }
 }
