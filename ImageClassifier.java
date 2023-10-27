66c3c5ec3d64f248d0a5514f541e1e391453fcc0
cfandoh123 <138801418+cfandoh123@users.noreply.github.com>
2023-10-27 17:58:50 -0400

    ImageClassifier.java

--- ImageClassifier.java
+++ ImageClassifier.java
@@ -1,99 +1,64 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class ImageClassifier {
 
+    // Creates a feature vector (1D array) from the given picture.
     public static double[] extractFeatures(Picture picture) {
-        // Extract features from the picture and return as a 1D array
-        // You can implement this function to convert the picture to a feature vector.
-        return null;
+
+        int width = picture.width(), height = picture.height();
+        double[] features = new double[width * height]; // store flattened matrix data
+        int idx = 0;  // linear array index for 'features'
+        for (int row = 0; row < height; row++)
+            for (int col = 0; col < width; col++)
+                features[idx++] = picture.get(col, row).getGreen();
+
+        return features;
     }
 
+    // See below.
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: ImageClassifier <training_file> <testing_file> <output_file>");
-            System.exit(1);
+        In trainingIn = new In(args[0]);
+        In testingIn = new In(args[1]);
+
+        int trainingClassCount, trainingPicHeight, trainingPicWidth;
+        trainingClassCount = trainingIn.readInt();
+        trainingPicHeight = trainingIn.readInt();
+        trainingPicWidth = trainingIn.readInt();
+
+        MultiPerceptron perceptron =
+                new MultiPerceptron(trainingClassCount,
+                        trainingPicWidth * trainingPicHeight);
+
+        while (!trainingIn.isEmpty()) {
+            String filename = trainingIn.readString();
+            int label = trainingIn.readInt();
+            double[] trainingInput = extractFeatures(new Picture(filename));
+
+            perceptron.trainMulti(trainingInput, label);
         }
 
-        // Parse command line arguments
-        String trainingFile = args[0];
-        String testingFile = args[1];
-        String outputFile = args[2];
+        // Prediction
 
-        // Read training data from the training file
-        MultiPerceptron multiPerceptron = null;
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(trainingFile));
-            int numClasses = Integer.parseInt(br.readLine());
-            String[] imageSize = br.readLine().split(" ");
-            int width = Integer.parseInt(imageSize[0]);
-            int height = Integer.parseInt(imageSize[1]);
-            multiPerceptron = new MultiPerceptron(numClasses, width * height);
+        // we know that the classCount and pic size is the same in training and
+        // in testing so we ignore that compononent from the testing file
+        // int testingClassCount, testingPicHeight, testingPicWidth;
+        // testingClassCount = trainingIn.readInt();
+        // testingPicHeight = trainingIn.readInt();
+        // testingPicWidth = trainingIn.readInt();
+        trainingIn.readInt();
+        trainingIn.readInt();
+        trainingIn.readInt();
+        int total = 0, right = 0;
+        while (!testingIn.isEmpty()) {
+            String filename = testingIn.readString();
+            int label = testingIn.readInt();
+            double[] trainingInput = extractFeatures(new Picture(filename));
 
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                String imageFile = parts[0];
-                int label = Integer.parseInt(parts[1]);
-                Picture picture = new Picture(imageFile);
-                double[] features = extractFeatures(picture);
-                multiPerceptron.trainMulti(features, label);
-            }
-            br.close();
-        } catch (IOException e) {
-            System.err.println("Error reading training data: " + e.getMessage());
-            System.exit(1);
+            int predictedVal = perceptron.predictMulti(trainingInput);
+            total++;
+            if (predictedVal == label) right++;
+
+            StdOut.println(filename + ", label = " + label + ", predict = " + predictedVal);
         }
 
-        // Read testing data from the testing file and classify
-        int correct = 0;
-        int total = 0;
-        StringBuilder misclassifiedImages = new StringBuilder();
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(testingFile));
-            String line;
-
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                String imageFile = parts[0];
-                int label = Integer.parseInt(parts[1]);
-                Picture picture = new Picture(imageFile);
-                double[] features = extractFeatures(picture);
-                int prediction = multiPerceptron.predictMulti(features);
-
-                if (prediction == label) {
-                    correct++;
-                } else {
-                    misclassifiedImages.append(imageFile).append(" (Predicted: ").append(prediction).append(", Actual: ").append(label).append(")\n");
-                }
-
-                total++;
-            }
-            br.close();
-        } catch (IOException e) {
-            System.err.println("Error reading testing data: " + e.getMessage());
-            System.exit(1);
-        }
-
-        // Calculate the test error rate
-        double errorRate = 1.0 - (double) correct / total;
-
-        // Output results to the specified output file
-        try {
-            String result = "Test Error Rate: " + String.format("%.2f%%", errorRate * 100) + "\n";
-            if (misclassifiedImages.length() > 0) {
-                result += "Misclassified Images:\n" + misclassifiedImages.toString();
-            }
-
-            Files.write(Paths.get(outputFile), result.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            System.err.println("Error writing to output file: " + e.getMessage());
-            System.exit(1);
-        }
+        StdOut.println("test error rate = " + (double) (total - right) / total);
     }
 }
